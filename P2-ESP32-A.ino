0928d5e97be88a6392463ef909643045c111916f
Jose031 <90293076+Jose031@users.noreply.github.com>
2021-11-09 18:44:58 -0600

    Configuración de inicialización del sensor BPM

--- P2-ESP32-A.ino
+++ P2-ESP32-A.ino
@@ -23,8 +23,24 @@ MAX30105 particleSensor;
 //**********************************************************************************************************************
 // Configuracion
 //**********************************************************************************************************************
+void setup()
+{
   Serial.begin(115200); // Velocidad con la que se trabja la comunicación serial del canal 0.
   Serial2.begin(115200);  // Velocidad con la que se trabaja la comunicación serial del canal 2.
+  
+  //------------------------ Configuración del Sensor de ritmo cárdiaco ------------------------
+  Serial.println("Initializing...");
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println("MAX30105 was not found. Please check wiring/power. ");
+    while (1);
+  }
+  Serial.println("Place your index finger on the sensor with steady pressure.");
+  particleSensor.setup(); //Configure sensor with default settings
+  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
+  //---------------------------------------------------------------------------------------------
+}
 //**********************************************************************************************************************
 // LOOP
 //**********************************************************************************************************************
